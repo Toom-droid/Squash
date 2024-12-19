ae6b65c540664da348a1d4256177ef5afcdb99fb
Tomas Anastacio <tomaslautaroan@gmail.com>
2024-12-18 23:44:26 -0300

    Models commited

---
+++ Squash.WebAPI/Models/Url.cs
@@ -0,0 +1,14 @@
+﻿namespace Squash.WebAPI.Models
+{
+    public class Url
+    {
+        public required int Id { get; set; } 
+        public required Guid Guid { get; set; } 
+        public required string OriginalUrl { get; set; } 
+        public required string ShortUrl { get; set; } 
+        public required int UserId { get; set; } 
+        public required User User { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required int VisitCount { get; set; } = 0;
+    }
+}
---
+++ Squash.WebAPI/Models/UrlAlias.cs
@@ -0,0 +1,14 @@
+﻿namespace Squash.WebAPI.Models
+{
+    public class UrlAlias
+    {
+        public required int Id { get; set; } 
+        public required Guid Guid { get; set; } 
+        public required string Alias { get; set; } 
+        public required int UrlId { get; set; } 
+        public required Url Url { get; set; } 
+        public required int UserId { get; set; } 
+        public required User User { get; set; }
+        public required DateTime CreatedAt { get; set; }
+    }
+}
