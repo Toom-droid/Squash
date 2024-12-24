ab9e4ec54a88ef42973bde3b3bfe1bc3d112ba74
Tomas Anastacio <tomaslautaroan@gmail.com>
2024-12-24 00:00:13 -0300

    Merge

---
+++ Squash.WebAPI/Program.cs
@@ -0,0 +1,112 @@
+using Microsoft.EntityFrameworkCore;
+using Squash.WebAPI.Repositories;
+using Squash.WebAPI.Services;
+using Squash.WebAPI.Interfaces.Repositories;
+using Squash.WebAPI.Interfaces.Services;
+using Squash.WebAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configure Entity Framework Core with SQL Server
+builder.Services.AddDbContext<SquashDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
+
+// Register Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+
+// Register Services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUrlService, UrlService>();
+
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(Squash.WebAPI.Mappings.MappingProfile));
+
+// Add Controllers
+builder.Services.AddControllers();
+
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure CORS (Allow only the specific frontend origin in production)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontendOrigin", policy =>
+    {
+        policy.WithOrigins("https://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials(); 
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+// Configure Kestrel for use HTTPS
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureHttpsDefaults(configureOptions =>
+    {
+        configureOptions.SslProtocols = System.Security.Authentication.SslProtocols.Tls13;
+    });
+});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 80); 
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+
+app.UseHttpsRedirection();
+
+
+app.UseCors(builder =>
+    builder.WithOrigins("https://localhost:4200")
+           .AllowCredentials()
+           .AllowAnyHeader()
+           .AllowAnyMethod());
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Cross-Origin-Opener-Policy", "same-origin");
+    context.Response.Headers.Add("Cross-Origin-Embedder-Policy", "require-corp");
+    await next();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
